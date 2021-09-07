d849cb22d2a5ea78a93b89c59f969e2305239212
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2021-09-07 23:17:07 +0530

    Update click_update.py

--- python_tkinter/click_update.py
+++ python_tkinter/click_update.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep  7 19:05:48 2021
 
-@author: nkele
-"""
 
 import tkinter
 import time
@@ -23,21 +18,3 @@ root.mainloop()
 
 
 
-from tkinter import *
-import datetime
-
-root = Tk()
-
-lab = Label(root)
-lab.pack()
-
-def clock():
-    time = datetime.datetime.now().strftime("Time: %H:%M:%S")
-    lab.config(text=time)
-    #lab['text'] = time
-    root.after(1000, clock) # run itself again after 1000 ms
-
-# run first time
-clock()
-
-root.mainloop()
